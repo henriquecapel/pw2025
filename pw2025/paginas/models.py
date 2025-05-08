11b051c8677cb15ea8485be0a9a980366feb3a1f
unknown <henriquecapel08@gmail.com>
2025-05-08 10:51:02 -0300

    Criação de views, urls e template

--- pw2025/paginas/models.py
+++ pw2025/paginas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class (models.Model):
+
+    
+
+    class Curso:
+        verbose_name = _("teste")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
--- pw2025/paginas/views.py
+++ pw2025/paginas/views.py
@@ -1,6 +1,21 @@
 from django.views.generic import TemplateView
 
+
 class Inicio(TemplateView):
     template_name = "paginas/index.html"
+    from django.views.generic import Campus
     
-    
+    class CampusCreate(CreateView):
+
+        template_name = 'paginas/form.html'
+        model = Campus
+        fields = ['nome']
+        sucess_url = reverse_lazy('index')
+
+    class CursoCreate(CreateView):
+        template_name = 'paginas/form.html'
+        model = Curso
+        fields = ['nome']
+        sucess_url = reverse-lazy('index')
+
+""
