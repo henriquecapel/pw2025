6a8ef159076e25d521a12ed3b7240dbb9754684d
henriquecapel <henriquecapel08@gmail.com>
2025-06-26 11:47:51 -0300

    Add files via upload

---
+++ pw2025-main/pw2025-main/pw2025-main atualizado/pw2025-main/paginas/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Cliente)
+admin.site.register(Fotografo)
+admin.site.register(Estudio)
+admin.site.register(SessaoFoto)
+
