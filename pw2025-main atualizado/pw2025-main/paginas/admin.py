f935c7999582ed8532c3cd26deb1635565cecc70
henriquecapel <henriquecapel08@gmail.com>
2025-07-31 12:04:21 -0300

    Delete pw2025-main atualizado/pw2025-main directory

--- pw2025-main atualizado/pw2025-main/paginas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Cliente)
-admin.site.register(Fotografo)
-admin.site.register(Estudio)
-admin.site.register(SessaoFoto)
-
